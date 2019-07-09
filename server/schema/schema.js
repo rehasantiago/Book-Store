c1c7c94ea343204bc5fc260b19a2e91e6864b3ab
feministAF <santiagoreha24@gmail.com>
2019-07-09 13:46:25 +0530

    added mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -84,8 +84,45 @@ const RootQuery = new GraphQLObjectType({
             }
         }
     }
-})
+});
 
+const Mutation = new GraphQLObjectType({
+    name:'Mutation',
+    fields:{
+        addAuthor:{
+            type:AuthorType,
+            args:{
+                name:{type:GraphQLString},
+                age:{type:GraphQLInt}
+            },
+            resolve(parent,args){
+                let author = new Author({
+                    name:args.name,
+                    age:args.age
+                })
+                return author.save();
+            }
+        },
+        addBook:{
+            type:BookType,
+            args:{
+                name:{type:GraphQLString},
+                genre:{type:GraphQLString},
+                authorId:{type:GraphQLID}
+            },
+            resolve(parent,args){
+                let book = new Book({
+                    name:args.name,
+                    genre:args.genre,
+                    authorId:args.authorId
+                });
+                return book.save();
+                
+            }
+        }
+    }
+})
 module.exports = new GraphQLSchema({
-    query:RootQuery
+    query:RootQuery,
+    mutation:Mutation
 })
