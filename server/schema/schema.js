f75badf6115f6df26ecaf3bc14581070ebf364f2
feministAF <santiagoreha24@gmail.com>
2019-07-09 16:50:24 +0530

    dealing with null values

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,7 +3,7 @@ const _ = require('lodash');
 const Book = require('../models/book');
 const Author = require('../models/author');
 
-const {GraphQLObjectType,GraphQLString,GraphQLSchema,GraphQLID,GraphQLInt,GraphQLList} = graphql;
+const {GraphQLObjectType,GraphQLString,GraphQLSchema,GraphQLID,GraphQLInt,GraphQLList,GraphQLNonNull} = graphql;
 
 //dummy data
 var books = [
@@ -98,8 +98,8 @@ const Mutation = new GraphQLObjectType({
         addAuthor:{
             type:AuthorType,
             args:{
-                name:{type:GraphQLString},
-                age:{type:GraphQLInt}
+                name:{type:new GraphQLNonNull(GraphQLString)},
+                age:{type:new GraphQLNonNull(GraphQLInt)}
             },
             resolve(parent,args){
                 let author = new Author({
@@ -112,9 +112,9 @@ const Mutation = new GraphQLObjectType({
         addBook:{
             type:BookType,
             args:{
-                name:{type:GraphQLString},
-                genre:{type:GraphQLString},
-                authorId:{type:GraphQLID}
+                name:{type:new GraphQLNonNull(GraphQLString)},
+                genre:{type:new GraphQLNonNull(GraphQLString)},
+                authorId:{type:new GraphQLNonNull(GraphQLID)}
             },
             resolve(parent,args){
                 let book = new Book({
