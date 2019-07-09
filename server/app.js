471cd140ec9bc12c41d1052e83c504d16b93bd76
feministAF <santiagoreha24@gmail.com>
2019-07-09 11:44:42 +0530

    connected to db

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');//graphqlHTTP is the fucking convention
 const schema = require('./schema/schema');
-
+const mongoose = require('mongoose');
 const app = express();
+
+//connecting to the db
+mongoose.connect('mongodb+srv://Reha:Rehasantiago24@bookstore-p5lwo.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true});
+mongoose.connection.once('open',() => {
+    console.log('connected to db');
+});//once the connection is open log the following to the console
 
 //the fn will handle any request made to /graphql
 app.use('/graphql',graphqlHTTP({
