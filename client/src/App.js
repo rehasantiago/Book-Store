fa5afd93cf3dd36980aada719d5b4bde89b4c70a
feministAF <santiagoreha24@gmail.com>
2019-07-10 04:34:18 +0530

    apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React,{Component} from 'react';
+import BookList from './components/BookList';
+import ApolloClient from "apollo-boost";
+import ApolloProvider from "react-apollo";
 
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+const client = new ApolloClient({
+  uri:'http://localhost:4000/graphql'
+})
+
+class App extends Component {
+  render(){
+    return (
+      <ApolloProvider client={client}
+      <div id="main">
+        <h1>Book Store</h1>
+        <BookList/>  
+      </div>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
