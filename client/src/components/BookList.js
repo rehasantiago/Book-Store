7c1837bcca3a59e2ef89aba153bf4d5108f87abf
feministAF <santiagoreha24@gmail.com>
2019-07-10 20:27:03 +0530

    completely done with book details

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -4,6 +4,13 @@ import {getBooksQuery} from '../queries/queries'
 import BookDetails from '../components/BookDetails';
 
 class BookList extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      selected:null
+    }
+  }
+
   displayBooks(){
     var data = this.props.data;
     if(data.loading){
@@ -12,7 +19,7 @@ class BookList extends Component {
     else{
       return data.books.map(book => {
         return(
-          <li key={book.id}>{ book.name }</li>
+          <li key={book.id} onClick = {(e) => {this.setState({selected:book.id})}}>{ book.name }</li>
         )
       })
     }
@@ -25,7 +32,7 @@ class BookList extends Component {
         <ul id="book-list">
             {this.displayBooks()}
         </ul>
-        <BookDetails />
+        <BookDetails bookId={this.state.selected}/>
       </div>
     );
   }
