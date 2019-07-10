d37284d2eaffa7827c9a077e433aaa744614fa01
feministAF <santiagoreha24@gmail.com>
2019-07-10 13:32:34 +0530

    displayed books

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -12,12 +12,26 @@ const getBooksQuery = gql`
 `
 
 class BookList extends Component {
+  displayBooks(){
+    var data = this.props.data;
+    if(data.loading){
+      return (<div>loading books...</div>);
+    }
+    else{
+      return data.books.map(book => {
+        return(
+          <li key={book.id}>{ book.name }</li>
+        )
+      })
+    }
+  }
+
   render(){
     //console.log(this.props);
     return (
       <div>
         <ul id="book-list">
-            <li>Book name</li>
+            {this.displayBooks()}
         </ul>
       </div>
     );
