e1070ebffe672c2e8b061a9195ced3019facd214
feministAF <santiagoreha24@gmail.com>
2019-07-10 15:57:30 +0530

    updated states

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -3,6 +3,15 @@ import {graphql} from 'react-apollo';
 import {getAuthorsQuery} from '../queries/queries'
 
 class AddBook extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name:"",
+            genre:"",
+            authorId:""
+        }
+    }
+
     displayAuthor(){
         var data = this.props.data;
         if(data.loading){
@@ -14,23 +23,27 @@ class AddBook extends Component {
             })
         }
     }
-  
+    
+    submitForm(e){
+        e.preventDefault();
+        console.log(this.state);
+    }
     render(){
       //console.log(this.props);
       return (
         <div>
-          <form id="add-book">
+          <form id="add-book" onSubmit={this.submitForm.bind(this)}>
             <div className="field">
                 <label>Book name:</label>
-                <input type="text"/>
+                <input type="text" onChange={(e) => this.setState({name:e.target.value})}/>
             </div>
             <div className="field">
                 <label>Genre</label>
-                <input type="text"/>
+                <input type="text" onChange={(e) => this.setState({genre:e.target.value})}/>
             </div>
             <div className="field">
                 <label>Author:</label>
-                <select>
+                <select onChange={(e) => this.setState({authorId:e.target.value})}>
                     {this.displayAuthor()}
                 </select>
             </div>
